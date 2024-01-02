e07b0260fd193076e55620b65eb337b0315f62db
Tim Lee <hwiorn@gmail.com>
2024-01-02 17:13:14 +0900

    test-bridge.rb

--- test-bridge.rb
+++ test-bridge.rb
@@ -24,11 +24,9 @@ require 'elrpc'
  # start a server process
 cl = Elrpc.start_process(["bundle", "exec", "ruby","edbi-bridge.rb"])
 
-puts "hhh"
  # synchronous calling
 #puts cl.call_method("connect", "dbi:SQLite3:/Users/gglee/test.sqlite", "", "")
 puts cl.call_method("connect", "dbi:SQLite3:/Users/gglee/test.sqlite", "", "")
-puts "ddd"
 
 #  # asynchronous calling
 # cl.call_method_async("echo", "3 world") do |err, value|
